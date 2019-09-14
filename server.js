0408ee495c31a1b11c9cb5021984be2656353ca8
Given Kafuwa <gkafuwa@gmail.com>
2019-09-14 12:58:22 +0200

    Added mongo

--- server.js
+++ server.js
@@ -1,10 +1,46 @@
-const io = require('socket.io').listen(8888).sockets();
+const io = require('socket.io').listen(8888);
+const mongo = require('mongodb').MongoClient;
+
+mongo.connect('mongodb://127.0.0.1/doppio', (err, db) => {
+    if (err) {
+        throw err
+    }
+    console.log('connected db');
+
+    let prod = db.collection('products');
+    let users = db.collection('users');
+    let tables = db.collection('tables');
+
+    io.on('connection', socket => {
+        console.log('connection is a go');
+
+        socket.on('getProds', (data) => {
+            prod.find({}).limit(100).sort({ _id: 1 }).toArray((err, res) => {
+                if (err) {
+                    throw err
+                }
+                socket.broadcast.emit('prods', res);
+            });
+        });
+
+        socket.on('callWaiter', (data) => {
+            console.log('something', data);
+            socket.broadcast.emit('WaiterCall', data); // emit an event to the socket
+
+        }); // listen to the event
+
+        socket.on('order', (data) => {
+            prod.insert({ name: name, message: message }, () => {
+                socket.broadcast.emit('output', [data]);
+                // send status object
+                sendStatus({
+                    message: "Message sent",
+                    clear: true
+                });
+            });
+        });
+
+    }); // end socket
 
 
-io.on('connection', socket => {
-    console.log('connection is a go');
-    socket.on('callWaiter', () => {
-        socket.emit('WaiterCall'); // emit an event to the socket
-
-    }); // listen to the event
 });
